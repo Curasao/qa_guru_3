b5f87433040be87c53ebdbc156312975b0a123aa
Darya <rudarya4@gmail.com>
2025-05-28 21:33:06 +0300

    Added homework 3

---
+++ test_selene.py
@@ -0,0 +1,25 @@
+import pytest
+from selene import browser, be, have
+
+
+@pytest.fixture(autouse=True)
+def browser_size():
+ browser.config.window_height = 1366
+ browser.config.window_width = 1024
+ browser.open('https://duckduckgo.com')
+ yield
+ browser.quit()
+
+
+def test_selene_find_text():
+ browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+ browser.element('html').should(have.text('Selene'))
+
+
+
+
+def test_selene_type_text():
+ browser.element('[name="q"]').should(be.blank).type('sadhjlasfhljsdaf').press_enter()
+ browser.element('html').should(have.text('По запросу «sadhjlasfhljsdaf» ничего не найдено.'))
+
+
